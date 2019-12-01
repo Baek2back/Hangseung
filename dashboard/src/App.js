6b0f55c1d9136da40a0d76f7fa67f129e70ed092
김성백 <>
2019-12-01 19:45:11 +0900

    CRA import antDesign with dark theme

---
+++ dashboard/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Button } from "antd";
+import { Switch } from "antd";
+import "antd/dist/antd.less";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Button>Button</Button>
+        <Switch></Switch>
+      </header>
+    </div>
+  );
+}
+
+export default App;
